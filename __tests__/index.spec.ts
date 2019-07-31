60f2308bd90e8fcd22a6df738f1332c98d5f6482
Beta Faccion <betafcc@gmail.com>
2019-07-31 11:31:06 -0300

    Adds tests

--- __tests__/index.spec.ts
+++ __tests__/index.spec.ts
@@ -1,19 +1,19 @@
 import { Cnpj } from '../src'
 
 describe(Cnpj.isValid, () => {
-  test('Com pontuação', () => expect(Cnpj.isValid('453.178.287-91')).toBe(true))
+  test('Com pontuação', () => expect(Cnpj.isValid('31.214.261/0001-38')).toBe(true))
 
-  test('Sem pontuação', () => expect(Cnpj.isValid('45317828791')).toBe(true))
+  test('Sem pontuação', () => expect(Cnpj.isValid('31214261000138')).toBe(true))
 
   test('Dígito verificador inválido', () =>
-    expect(Cnpj.isValid('45317828792')).toBe(false))
+    expect(Cnpj.isValid('31.214.261/0001-39')).toBe(false))
 })
 
 describe(Cnpj.from, () => {
   test('Possíveis assinaturas', () => {
-    const formatted = Cnpj.from('453.178.287-91')
-    const stripped = Cnpj.from('45317828791')
-    const withoutDvs = Cnpj.from('453.178.287')
+    const formatted = Cnpj.from('31.214.261/0001-38')
+    const stripped = Cnpj.from('31214261000138')
+    const withoutDvs = Cnpj.from('31.214.261/0001')
 
     expect(formatted.equals(stripped) && formatted.equals(withoutDvs)).toBe(true)
   })
@@ -24,11 +24,15 @@ describe(Cnpj.random, () => {
 })
 
 describe(Cnpj, () => {
-  test('strip', () => expect(Cnpj.from('453.178.287-91').strip()).toBe('45317828791'))
-  test('format', () => expect(Cnpj.from('45317828791').format()).toBe('453.178.287-91'))
+  test('strip', () =>
+    expect(Cnpj.from('31.214.261/0001-38').strip()).toBe('31214261000138'))
+
+  test('format', () =>
+    expect(Cnpj.from('31214261000138').format()).toBe('31.214.261/0001-38'))
+
   test('equals', () => {
-    const formatted = Cnpj.from('453.178.287-91')
-    const stripped = Cnpj.from('45317828791')
+    const formatted = Cnpj.from('31.214.261/0001-38')
+    const stripped = Cnpj.from('31214261000138')
 
     expect(formatted.equals(stripped)).toBe(true)
   })
